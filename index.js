68447fa3e3223f7d29331a2178785dd74d0b864a
mkuch2 <179979492+mkuch2@users.noreply.github.com>
2024-12-15 16:14:46 -0800

    Create dialog to add book
    User can now dynamically add books using add book button

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const bookGrid = document.querySelector(".container");
+const addBookBtn = document.querySelector("#add-book");
+const dialog = document.querySelector("dialog");
+const dialogSubmitBtn = document.querySelector("#dialog--submit-btn");
+const dialogAuthor = document.querySelector("#dialog-author");
+const dialogTitle = document.querySelector("#dialog-title");
+const dialogPages = document.querySelector("#dialog-pages");
+const dialogRead = document.querySelector("#dialog-read");
 
 const myLibrary = [];
 function Book(title, author, pages, read) {
@@ -13,6 +20,10 @@ function addBookToLibrary(book) {
 }
 
 function displayBooks() {
+  while(bookGrid.firstChild){
+    bookGrid.removeChild(bookGrid.lastChild);
+  }
+
   myLibrary.forEach((book) => {
     //create book div
     const div = document.createElement("div");
@@ -31,10 +42,36 @@ function displayBooks() {
     div.appendChild(cardPages);
 
     const cardReadCheck = document.createElement("p");
-    this.book === true ? cardReadCheck.textContent = "Read" : cardReadCheck.textContent = "Unread";
+    book.read === true ? cardReadCheck.textContent = "Read" : cardReadCheck.textContent = "Unread";
     div.appendChild(cardReadCheck);
 
     bookGrid.appendChild(div);
   });
 }
 
+addBookBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+
+dialogSubmitBtn.addEventListener("click", (e)=>{
+  e.preventDefault();
+  if(dialogAuthor !== '' && dialogTitle.value !== ''
+      && dialogPages.value !== ''){
+    //create book object with specified inputs
+    const book = new Book(dialogAuthor.value, dialogTitle.value,
+      dialogPages.value, dialogRead.checked);
+
+    //add book to library and display
+    addBookToLibrary(book);
+    displayBooks();
+
+    //clear dialog contents
+    dialogAuthor.value = '';
+    dialogTitle.value = '';
+    dialogPages.value = '';
+    dialogRead.checked = false;
+
+    dialog.close()
+  }
+});
