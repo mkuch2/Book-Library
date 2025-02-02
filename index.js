6635c006d9c00068a2ed1098f9a00f85052fc8da
mkuch2 <179979492+mkuch2@users.noreply.github.com>
2025-02-01 19:02:29 -0800

    Add data persistence
    Use localStorage to keep library data after reloading site

--- index.js
+++ index.js
@@ -7,7 +7,23 @@ const dialogTitle = document.querySelector("#dialog-title");
 const dialogPages = document.querySelector("#dialog-pages");
 const dialogRead = document.querySelector("#dialog-read");
 
+function saveToLocalStorage() {
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+}
+
+function loadFromLocalStorage() {
+  const stored = localStorage.getItem("myLibrary");
+  if (stored) {
+    myLibrary = JSON.parse(stored).map((book) => {
+      return new Book(book.title, book.author, book.pages, book.read);
+    });
+    displayBooks();
+  }
+}
+
 const myLibrary = [];
+loadFromLocalStorage();
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -15,18 +31,16 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-Book.prototype.toggleRead = function(){
+Book.prototype.toggleRead = function () {
   this.read = !this.read;
-}
-
-
+};
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
 function displayBooks() {
-  while(bookGrid.firstChild){
+  while (bookGrid.firstChild) {
     bookGrid.removeChild(bookGrid.lastChild);
   }
 
@@ -39,7 +53,7 @@ function displayBooks() {
     cardTitle.textContent = book.title;
     div.appendChild(cardTitle);
 
-    const cardAuthor = document.createElement("p")
+    const cardAuthor = document.createElement("p");
     cardAuthor.textContent = book.author;
     div.appendChild(cardAuthor);
 
@@ -48,10 +62,11 @@ function displayBooks() {
     div.appendChild(cardPages);
 
     const cardReadCheck = document.createElement("p");
-    book.read === true ? cardReadCheck.textContent = "Read" : cardReadCheck.textContent = "Unread";
+    book.read === true
+      ? (cardReadCheck.textContent = "Read")
+      : (cardReadCheck.textContent = "Unread");
     cardReadCheck.classList.add("read-check");
     div.appendChild(cardReadCheck);
-
 
     const bookButtonContainer = document.createElement("div");
     bookButtonContainer.classList.add("book--button-container");
@@ -59,38 +74,35 @@ function displayBooks() {
     const deleteBook = document.createElement("button");
     deleteBook.textContent = "Delete book";
     deleteBook.classList.add("delete-book-btn");
-    
 
-    //add delete book button and functionality
-    deleteBook.addEventListener("click", ()=>{
+    deleteBook.addEventListener("click", () => {
       myLibrary.splice(position, 1);
-
+      saveToLocalStorage();
       displayBooks();
     });
 
     const readBook = document.createElement("button");
-    if(book.read){
-      readBook.textContent = 'Mark as unread';
-      cardReadCheck.classList.add('read');
-    }
-    else{
-      readBook.textContent = 'Mark as read';
-      readBook.classList.remove('read');
+    if (book.read) {
+      readBook.textContent = "Mark as unread";
+      cardReadCheck.classList.add("read");
+    } else {
+      readBook.textContent = "Mark as read";
+      readBook.classList.remove("read");
     }
     readBook.classList.add("read-book-btn");
     bookButtonContainer.appendChild(readBook);
     bookButtonContainer.appendChild(deleteBook);
 
-    readBook.addEventListener("click", ()=>{
+    readBook.addEventListener("click", () => {
       book.toggleRead();
-      cardReadCheck.classList.toggle('read');
-
-      readBook.textContent = book.read ? 'Mark as unread' : 'Mark as read';
-      cardReadCheck.textContent = book.read ? 'Read' : 'Unread';
+      cardReadCheck.classList.toggle("read");
+      readBook.textContent = book.read ? "Mark as unread" : "Mark as read";
+      cardReadCheck.textContent = book.read ? "Read" : "Unread";
+      saveToLocalStorage();
     });
+
     div.appendChild(bookButtonContainer);
     bookGrid.appendChild(div);
-
   });
 }
 
@@ -98,26 +110,32 @@ addBookBtn.addEventListener("click", () => {
   dialog.showModal();
 });
 
-
-dialogSubmitBtn.addEventListener("click", (e)=>{
+dialogSubmitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  if(dialogAuthor.value !== '' && dialogTitle.value !== ''
-      && dialogPages.value !== ''){
+  if (
+    dialogAuthor.value !== "" &&
+    dialogTitle.value !== "" &&
+    dialogPages.value !== ""
+  ) {
     //create book object with specified inputs
-    const book = new Book(dialogAuthor.value, dialogTitle.value,
-      dialogPages.value, dialogRead.checked);
+    const book = new Book(
+      dialogAuthor.value,
+      dialogTitle.value,
+      dialogPages.value,
+      dialogRead.checked
+    );
 
     //add book to library and display
     addBookToLibrary(book);
     displayBooks();
+    saveToLocalStorage();
 
     //clear dialog contents
-    dialogAuthor.value = '';
-    dialogTitle.value = '';
-    dialogPages.value = '';
+    dialogAuthor.value = "";
+    dialogTitle.value = "";
+    dialogPages.value = "";
     dialogRead.checked = false;
 
-    dialog.close()
+    dialog.close();
   }
 });
-
